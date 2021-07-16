72cbd2854d38a9fe25cfc09a8109f6c925d001c4
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-16 12:50:33 +0300

    Add dyamic style switching

--- src/js/View.mjs
+++ src/js/View.mjs
@@ -1,8 +1,36 @@
 import Element from './Element.mjs'
 
 export default class View extends Element {
-    constructor(args = {}) {
+    constructor({ args }) {
         super(args)
+    }
+
+    _createStyleLink(styleLink) {
+        const newStyleLink = document.createElement('link')
+        newStyleLink.rel = 'stylesheet'
+        newStyleLink.href = styleLink
+        newStyleLink.setAttribute('data-style', '')
+        document.head.append(newStyleLink)
+    }
+
+    importStyle(styleLink) {
+        if (!styleLink) return
+
+        const stylesToRemove = document.querySelectorAll('[data-style]')
+        stylesToRemove.forEach((style) => style.remove())
+
+        this._createStyleLink(styleLink)
+    }
+
+    importStyles(styleLinks) {
+        if (!styleLinks.length) return
+
+        const stylesToRemove = document.querySelectorAll('[data-style]')
+        stylesToRemove.forEach((style) => style.remove())
+
+        for (const styleLink of styleLinks) {
+            this._createStyleLink(styleLink)
+        }
     }
 
     /**
